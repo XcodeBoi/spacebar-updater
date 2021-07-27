c82fc3c41e8e3cf53b7511a02958c570bf876082
textEditUser <runningzoomcat@gmail.com>
2021-07-28 09:15:12 +1000

    seperate files & testing stuff

--- core.js
+++ core.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee"];
 let randIndex = Math.floor(Math.random() * names.length);
 console.log("Good day, " + names[randIndex])
-exec(`spacebar -m config left_shell_command "echo A good day to you, ${ names[randIndex] }"`,
+exec(`./spacebar-contact ${names[randIndex]}`,
     (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
