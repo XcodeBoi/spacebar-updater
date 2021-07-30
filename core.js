607242ef998593a411ffac79e02c82b66ac101ad
textEditUser <runningzoomcat@gmail.com>
2021-07-30 12:21:19 +1000

    remove error checking + add sqlite3 package

--- core.js
+++ core.js
@@ -1,13 +1,8 @@
 const { exec } = require('child_process');
-let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee"];
+let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee", "Alia"];
+let selection2 = ["balls", "nutsack"]
 let randIndex = Math.floor(Math.random() * names.length);
 console.log("Good day, " + names[randIndex])
-exec(`./spacebar-contact ${names[randIndex]}`,
-    (error, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-        if (error != null) {
-            console.log(`exec error: ${error}`);
-        }
-    });
-
+exec(`./spacebar-contact ${names[randIndex]}`)
+// haha error checking is for plebs
+// man this code is shit
