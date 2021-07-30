da92f4f8cc6d5d6495175536a8cf13982aa845c3
textEditUser <runningzoomcat@gmail.com>
2021-07-30 12:35:05 +1000

    merge spacebar interaction functionality back into node

--- core.js
+++ core.js
@@ -1,8 +1,8 @@
 const { exec } = require('child_process');
-let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee", "Alia"];
+let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee", "Alia", "adelaide"];
 let selection2 = ["balls", "nutsack"]
 let randIndex = Math.floor(Math.random() * names.length);
 console.log("Good day, " + names[randIndex])
-exec(`./spacebar-contact ${names[randIndex]}`)
+exec(`spacebar -m config left_shell_command "echo A good day to you, ${names[randIndex]}"`)
 // haha error checking is for plebs
 // man this code is shit
