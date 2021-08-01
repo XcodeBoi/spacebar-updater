2ef220455c34341335ca01e90ef5113aae4a3568
textEditUser <runningzoomcat@gmail.com>
2021-08-01 20:04:53 +1000

    first iteration of testing on chat.db

--- core.js
+++ core.js
@@ -1,8 +1,14 @@
 const { exec } = require('child_process');
-let names = ["Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee", "Alia", "adelaide"];
-let selection2 = ["balls", "nutsack"]
+let names = ["Ted", "Harri", "Willem", "Liam", "Hamish", "Minh-Quanieeeeee", "Alia", "Adelaide"];
 let randIndex = Math.floor(Math.random() * names.length);
 console.log("Good day, " + names[randIndex])
 exec(`spacebar -m config left_shell_command "echo A good day to you, ${names[randIndex]}"`)
 // haha error checking is for plebs
 // man this code is shit
+
+// unread message calculation
+
+exec(`sqlite3 ~/Library/Messages/chat.db "select count(*) from message"`, (error, stdout) => {
+    exec(`sqlite3 ./msg_count.db "insert into msg_stats (tracked) values (${stdout})"`)
+})
+
